4237fff22953021aee31166552e5499f486d2d6b
Terry <tsehowang@gmail.com>
2022-02-28 22:32:00 +0800

    create lab2 create lab2

---
+++ lib/src/main/java/comp3111/lab2/ex1/Book.java
@@ -0,0 +1,29 @@
+package comp3111.lab2.ex1;
+
+public class Book {
+	private String chapters[];
+	private static final int DEFAULT_CHAPTERS = 10;
+	
+	public Book() {
+		chapters = new String[DEFAULT_CHAPTERS];
+		for (int i = 0; i < chapters.length; i++) {
+			chapters[i] = "n/a";
+		}
+	}
+	
+	public Book(String argument[]) {
+		/* construct the object with an array of chapter names */
+		/* ADD YOUR CODE HERE */
+		chapters = argument;
+	}
+	
+	public String getChapters(int i) {
+		/* return the chapter by the given index */
+		/* ADD YOUR CODE HERE */
+		return chapters[i];
+	}
+
+	public String[] getChapters() {
+		return chapters;
+	}
+}
---
+++ lib/src/main/java/comp3111/lab2/ex2/Charger.java
@@ -0,0 +1,12 @@
+package comp3111.lab2.ex2;
+
+interface Chargeable {
+	public void charge();
+}
+
+public class Charger {
+	public void charge(Chargeable c) {
+		c.charge();
+	}
+
+}
